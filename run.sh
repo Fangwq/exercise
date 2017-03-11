6f48964af0126b8eb9ee839c3c71738d999b8c98
FWQ <shenzhizi124@gmail.com>
2017-03-11 19:22:04 +0800

    shell script to run the code

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# I still need to enter a file name, it's boring
+#echo -n "please enter the file name with pyx extension: "
+#read name
+#If I run the code with ./run.sh filename, then don't need to read
+file=$1
+echo -n "Begin to run the code, wait for a moment..."
+printf "\n"
+cython $file.pyx
+gcc -c -fPIC -I/usr/include/python2.7/ $file.c
+gcc -shared $file.o -o $file.so -lpython2.7
+python $file.py
+# ====delete the temporary file=======
+rm  $file.c
+rm  $file.o
+rm  $file.so
+echo -n "The running finished!"
+printf "\n"
+
