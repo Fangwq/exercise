ab89ade3f34a7d3eeadddeec8ded426dced2f640
FWQ <shenzhizi124@gmail.com>
2017-03-18 23:57:42 +0800

    $3

--- commit.sh
+++ commit.sh
@@ -2,8 +2,8 @@
 echo 'the code aims to push code to github'
 echo '=========='
 echo "$1, $2, $3"
-git add $x.$y
-git ci -m "$z"
+git add $1.$2
+git ci -m '$3'
 git push origin master
 echo "=========="
 echo "It is over!"
